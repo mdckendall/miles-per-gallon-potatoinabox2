901292cd5f37b8b93d0ba70832d1324b814810d1
Andres <108771127+potatoinabox2@users.noreply.github.com>
2023-04-13 23:18:50 +0000

    i think i understood the directions

--- main.py
+++ main.py
@@ -3,3 +3,7 @@
 #read in the miles and read in the gallons used.
 
 #display the miles per gallon
+
+print('your code here!')
+print('read in the miles and read in the gallons used.')
+print ('display the miles per gallon')
